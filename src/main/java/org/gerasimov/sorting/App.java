7176ef93277c3a6b8527788852dc34f041a41e50
Anton_Gerasimov <antonsergeevichgerasimov@gmail.com>
2023-07-27 11:02:57 +0400

    Add logs

--- src/main/java/org/gerasimov/sorting/App.java
+++ src/main/java/org/gerasimov/sorting/App.java
@@ -1,10 +1,21 @@
 package org.gerasimov.sorting;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class App {
+
     public static void main(String[] args){
+
+        final Logger LOGGER = Logger.getLogger(ArraySorter.class.getName());
+
         ArraySorter arraySorter = new ArraySorter();
 
-        String[] testString = {"123", "32"};
-        arraySorter.sort(testString);
+        try{
+            System.out.println(arraySorter.sort(args));
+        } catch(IllegalArgumentException e){
+            LOGGER.log(Level.SEVERE, e.getMessage());
+        }
+
+
     }
 }
